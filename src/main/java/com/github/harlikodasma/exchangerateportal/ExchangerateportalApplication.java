f4a349a8e12baa932ad442ceb169ef39602b9eec
Harli Kodasma <harlikodasma@users.noreply.github.com>
2023-02-20 00:07:40 +0200

    Initial commit

---
+++ src/main/java/com/github/harlikodasma/exchangerateportal/ExchangerateportalApplication.java
@@ -0,0 +1,13 @@
+package com.github.harlikodasma.exchangerateportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangerateportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangerateportalApplication.class, args);
+	}
+
+}
